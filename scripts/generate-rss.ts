da0afe1c89a290317781870a0c13d69d21483076
Robin Metral <robin.metral@sumup.com>
2021-01-23 00:49:56 +0100

    feat: generate RSS for book reviews on postbuild

---
+++ scripts/generate-rss.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import { Feed } from "feed";
+
+import { getBooksSlugs, getBookBySlug, markdownToHtml } from "../lib/books";
+
+async function generateRSS() {
+  const feed = new Feed({
+    // required params
+    title: "Robin's Book Reviews",
+    description: "Recent book reviews on robinmetral.com",
+    link: "https://robinmetral.com/books",
+    // optional params
+    language: "en",
+    // id and copyright should be optional
+    // see https://github.com/jpmonette/feed/issues/138
+    id: "some-id",
+    copyright: "Robin Métral 2021",
+    feedLinks: {
+      rss: "https://robinmetral.com/books/feed.xml",
+    },
+  });
+
+  const slugs = getBooksSlugs();
+  const books = await Promise.all(
+    slugs.map(async (slug) => {
+      const { frontMatter, markdown } = getBookBySlug(slug);
+      const html = await markdownToHtml(markdown);
+      return { frontMatter, html, slug };
+    })
+  );
+
+  books
+    .filter(({ frontMatter }) => !!frontMatter.finishedDate)
+    .sort(
+      (a, b) =>
+        new Date(a.frontMatter.finishedDate).valueOf() -
+        new Date(b.frontMatter.finishedDate).valueOf() // .valueOf is a TS fix
+    )
+    .map(({ frontMatter, html, slug }) => {
+      feed.addItem({
+        title: frontMatter.title,
+        link: `https://robinmetral.com/books/${slug}`,
+        date: new Date(frontMatter.finishedDate),
+        content: html,
+      });
+    });
+
+  const rss = feed.rss2();
+  fs.writeFileSync("./public/books/feed.xml", rss);
+}
+
+generateRSS();

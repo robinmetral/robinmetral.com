1877e1acf13b2b5b5bc9aabb05654f340da8f021
Robin Métral <robin@metral.ch>
2021-01-26 15:27:21 +0100

    Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,22 @@ export default function Home() {
   return (
     <>
       <Heading>
-        Hi<span className="text-yellow-400">.</span>
+        Hi<span className="text-yellow-400">,</span>
       </Heading>
       <p className="mb-4">My name is Robin.</p>
+      <p className="mb-4">
+        I grew up in Geneva, Switzerland, and I'm currently living in Berlin. I
+        work as a software engineer at{" "}
+        <Link href="https://sumup.com">SumUp</Link>.
+      </p>
+      <p className="mb-4">
+        I'm aware this isn't much of a bio, and I promise I'll add some more
+        soon.
+      </p>
       <p>
-        I'm redesigning my website (again). For updates, you can subscribe to my{" "}
-        <Link href="/newsletter">newsletter</Link> or follow me on{" "}
-        <Link href="https://twitter.com/robinmetral">Twitter</Link>.
+        Until then, feel free to take a look around,{" "}
+        <Link href="/newsletter">subscribe</Link> to my newsletter, or{" "}
+        <Link href="https://twitter.com/robinmetral">say hi</Link> on Twitter.
       </p>
     </>
   );

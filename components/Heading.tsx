64dde64340fe1ab1ca5afecefef1885b0878ae46
Robin Métral <robin@metral.ch>
2021-01-26 15:00:33 +0100

    Add h2 to Heading component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,5 +1,16 @@
 import { ReactNode } from "react";
 
-export default function Heading({ children }: { children: ReactNode }) {
-  return <h1 className="text-6xl md:text-8xl font-black mb-4">{children}</h1>;
+export default function Heading({
+  children,
+  level = "1",
+}: {
+  children: ReactNode;
+  level?: "1" | "2";
+}) {
+  if (level === "1") {
+    return <h1 className="text-6xl md:text-8xl font-black mb-4">{children}</h1>;
+  }
+  if (level === "2") {
+    return <h2 className="text-4xl md:text-6xl font-black mb-4">{children}</h2>;
+  }
 }

9e42b0cf48d8470c180fc92914f4634118e2378e
Robin Métral <robin@metral.ch>
2021-03-24 08:50:03 +0100

    feat: add Splitbee analytics (#9)

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   images: {
     domains: ["covers.openlibrary.org"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 };

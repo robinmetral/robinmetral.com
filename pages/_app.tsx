3f2c965b34d613065e8515c415303750e28f5bce
Robin Métral <robin@metral.ch>
2021-03-24 08:46:38 +0100

    feat: add Splitbee analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from "next/app";
+import splitbee from "@splitbee/web";
 
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+
+splitbee.init({
+  scriptUrl: "/bee.js",
+  apiUrl: "/_hive",
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
